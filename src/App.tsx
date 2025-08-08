8a21745196cda21fc344549a3943c0036c0a42f8
Joseph <jwoleabu@gmail.com>
2025-08-08 22:39:02 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { TranscriptParser } from './responder'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const parser = new TranscriptParser
+
+  const onClick = async () =>{
+    parser.hello()
+  }
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Transcript Search</h1>
+      <div className="card">
+        <button onClick={onClick}>
+          count is {1}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/responder.ts
@@ -0,0 +1,6 @@
+export class TranscriptParser{
+
+  hello(): void{
+    console.log("hello")
+	}
+}
